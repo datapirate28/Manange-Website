58983d3c09471e9732562097e31e138965f16a61
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-05-31 00:07:33 +0300

    Completed the website

--- app.js
+++ app.js
@@ -9,3 +9,43 @@ navToggle.addEventListener("click", () => {
     primaryHeader.toggleAttribute("data-overlay");
 })
 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: '1',
+    spaceBetween: 30,
+    hashNavigation: {
+      watchState: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        440: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        800: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+        850: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+        },
+        1220: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+
+        1420: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+      }
+  });
+
